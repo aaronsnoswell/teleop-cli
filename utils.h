81ece61c32ca58120c3df3ed06942424dd7b48bc
Aaron Snoswell <aaronsnoswell@gmail.com>
2017-12-08 09:23:04 +1000

    Use operator() for matrix indexing

--- utils.h
+++ utils.h
@@ -14,30 +14,26 @@ using namespace chai3d;
  */
 cVector3d rotationMatrixToEulerAngles(cMatrix3d R, float singularThreshold = 1e-6)
 {
-	/* XXX ajs 7/Dec/2017 indices into values array might need
-	 * to be swapped below
-	 */
-	double values[3][3];
-	R.get((double **) values);
-
+	// XXX ajs 7/Dec/2017 indices into R might need to be swapped below
+	
 	double sy = sqrt(
-		values[0][0] * values[0][0] + values[1][0] * values[1][0]
+		R(0, 0) * R(0, 0) + R(1, 0) * R(1, 0)
 	);
 
 	double x, y, z;
 	if (sy < singularThreshold)
 	{
 		// Matrix is singular
-		x = atan2(-values[1][2], values[1][1]);
-		y = atan2(-values[2][0], sy);
+		x = atan2(-R(1, 2), R(1, 1));
+		y = atan2(-R(2, 0), sy);
 		z = 0;
 	}
 	else
 	{
 		// Matrix is not singular
-		x = atan2(values[2][1], values[2][2]);
-		y = atan2(-values[2][0], sy);
-		z = atan2(values[1][0], values[0][0]);
+		x = atan2(R(2, 1), R(2, 2));
+		y = atan2(-R(2, 0), sy);
+		z = atan2(R(1, 0), R(0, 0));
 	}
 
 	return cVector3d(x, y, z);
